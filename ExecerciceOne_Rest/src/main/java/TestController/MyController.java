f79488093262487504b5f3c95c160714973bd811
batoula1 <102437354+batoula1@users.noreply.github.com>
2024-08-27 03:14:21 -0400

    Almost-done

---
+++ ExecerciceOne_Rest/src/main/java/TestController/MyController.java
@@ -0,0 +1,25 @@
+package com.perscholas.TestController;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("home")
+public class MyController {
+
+    @RequestMapping({"/login"})
+    public String showLogin(){
+        return "inboxpage";
+    }
+
+    @RequestMapping("/")
+    public String simplemethod(){
+        return "inboxpage";
+    }
+
+    @RequestMapping("/index")
+    public String showindex(){
+        return "inboxpage";
+    }
+
+}

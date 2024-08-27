534e054efcc9a3c4dd0fbf42e749e2bfd6d54490
batoula1 <102437354+batoula1@users.noreply.github.com>
2024-08-27 03:35:15 -0400

    Almost done

---
+++ src/test/java/com/perscholas/ExecerciceOne_Rest/ExecerciceOneRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.perscholas.ExecerciceOne_Rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExecerciceOneRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
